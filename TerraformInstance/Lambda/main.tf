abe69a37ee7999bf39a6a51ee40bd7d559564a5b
Trupt Satishbhai Patel <128998581+truptpatel21@users.noreply.github.com>
2024-06-24 18:22:27 +0530

    Update main.tf

--- TerraformInstance/Lambda/main.tf
+++ TerraformInstance/Lambda/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_lambda_function" "example_lambda" {
-  function_name = "tp_lambda1"
+  function_name = "tp_lambda1pp"
   role          = aws_iam_role.lambda_exec.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
